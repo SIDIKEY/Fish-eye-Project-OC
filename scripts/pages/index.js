639ccf6b7a15e34de44eef71c9a3f2f932de573b
Sidiki Keita <sid@Sidikis-MacBook-Air.local>
2024-04-08 11:28:59 +0200

    fetch index page datas

---
+++ scripts/pages/index.js
@@ -0,0 +1,31 @@
+    async function getPhotographers() {
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+        try{
+            const response = await fetch("/data/photographers.json")
+            const data = await response.json();
+            return data;
+        }catch (error) {
+            console.error(error);
+            return ({photographers: []})
+        }
+        
+        }
+    async function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer_section");
+
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerTemplate(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
+        });
+    }
+
+    async function init() {
+        // Récupère les datas des photographes
+        const { photographers } = await getPhotographers();
+        displayData(photographers);
+    }
+    
+    init();
+    
---
+++ scripts/templates/photographer.js
@@ -0,0 +1,34 @@
+function photographerTemplate(data) {
+    const { name, portrait, id, tagline, city, country, price} = data;
+
+    const picture = `assets/photographers/${portrait}`;
+
+    function getUserCardDOM() {
+        const article = document.createElement('article');
+        const link = document.createElement('a');
+        link.href = `photographer.html?id=${id}`;
+
+        const img = document.createElement('img');
+        const h2 = document.createElement('h2');
+        const h3 = document.createElement('h3');
+        const h4 = document.createElement('h4');
+        const h5 = document.createElement("h5");
+        const linkDiv = document.createElement('div');
+        link.appendChild(linkDiv);
+        img.setAttribute("src", picture);
+        img.setAttribute('alt', name);
+        linkDiv.appendChild(img);    
+        link.appendChild(h2);
+        h2.textContent = name;
+        h3.textContent = city+ ", " + country;
+        h4.textContent = tagline;
+        h5.textContent = price + "€/jour";
+        article.appendChild(link);
+        article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(h5);
+        return article;
+    }
+    return { name, picture, tagline, price, city, country, getUserCardDOM }
+}
